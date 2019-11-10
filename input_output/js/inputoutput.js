afbaa6347411650ba903742d436ad6c13ffb72be
summmiii <xus057@newschool.edu>
2019-11-10 13:38:14 -0500

    js

--- input_output/js/inputoutput.js
+++ input_output/js/inputoutput.js
@@ -9,6 +9,6 @@ console.log(userOutputEl);
 
 //THESE TWO LINES DO THE SAME THIN
 //userOutputEl.innerText = "Hello. " + userInput+ "!";
-userOutputEl.innerText = `Hello, ${userInput}!`;
+userOutputEl.innerText = `Hello, ${userInput}! Move your mouse on me!`;
 
 document.body.onmousemove = function(){document.body.innerHTML += `<h1 class=mouse style="color:rgb(${Math.random()*175},${Math.random()*175},${Math.random()*175})">Your Mouse Just Moved</h1>`}
