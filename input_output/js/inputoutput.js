07cc95d2fc526fc715485a3e3f2da2c36536e8b2
summmiii <xus057@newschool.edu>
2019-11-19 19:13:52 -0500

    color

--- input_output/js/inputoutput.js
+++ input_output/js/inputoutput.js
@@ -1,7 +1,7 @@
 // console.log('--js loaded--');
 
 
-const userInput = window.prompt('What is your name?');
+const userInput = window.prompt('What is your favorite color?');
 const userOutputEl = document.getElementById('output');
 
 console.log(userInput);
@@ -9,6 +9,8 @@ console.log(userOutputEl);
 
 //THESE TWO LINES DO THE SAME THIN
 //userOutputEl.innerText = "Hello. " + userInput+ "!";
-userOutputEl.innerText = `Hello, ${userInput}! Move your mouse on me!`;
+userOutputEl.innerText = `YAY! ${userInput} is a nice taste!`;
+
+
 
 document.body.onmousemove = function(){document.body.innerHTML += `<h1 class=mouse style="color:rgb(${Math.random()*175},${Math.random()*175},${Math.random()*175})">Your Mouse Just Moved</h1>`}
